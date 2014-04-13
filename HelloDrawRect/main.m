b031fceaf221f68dee066eed6b2e237ff45208dd
Bear <timfanda35@gmail.com>
2014-04-13 09:51:42 +0800

    Initial Commit

---
+++ HelloDrawRect/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HelloDrawRect
+//
+//  Created by Bear on 2014/4/13.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
